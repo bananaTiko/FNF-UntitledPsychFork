8ee926ca2560a7296b51a3007be562fb4c81fc05
bananaTiko <111482265+bananaTiko@users.noreply.github.com>
2025-04-15 17:08:23 -0700

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -12,7 +12,7 @@ class OptionsState extends MusicBeatState
 		'Graphics',
 		'Visuals',
 		'Gameplay',
-		'Audio',
+		'Audio'
 		#if TRANSLATIONS_ALLOWED , 'Language' #end
 	];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
